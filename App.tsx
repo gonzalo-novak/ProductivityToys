fad297bef5037490fe1dc365fa470f771ec280ac
gonzalo-novak <monstertrap@protonmail.com>
2023-07-01 17:38:05 -0600

    chore: initial example using jotai for managing state across the application

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { atom, useAtom } from 'jotai';
+import { useCallback } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+const userAtomTest = atom<{ name: string, age: number } | null>(null);
 
 export default function App() {
+  const [user, setUser] = useAtom(userAtomTest);
+
+  const handleUserSession = useCallback(() => {
+    setUser(prevUser => prevUser ? null : { name: 'John Doe', age: 20 });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {(user) && <Text>Hello {user.name}, your age is: {user.age}</Text>}
+      <Button onPress={handleUserSession} title={(user) ? 'Log out' : 'Log in'} />
       <StatusBar style="auto" />
     </View>
   );
